024f2e6ee49c1bb954d2ded6f4739a4570dbe1d5
Alex Delgado <delgadoalex@icloud.com>
2019-12-30 11:52:20 -0600

    added components and pages folders - created RecordList component and working with the discogs api

---
+++ src/components/records-list/records-list.component.jsx
@@ -0,0 +1,53 @@
+import React from 'react'; 
+
+class RecordsList extends React.Component {
+    constructor() {
+        super(); 
+
+        this.state = {
+            error: null, 
+            isLoaded: false,
+            records: [], 
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://api.discogs.com/artists/1?callback=callbackname')
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true, 
+                        items: result.items
+                    }); 
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true, 
+                        error
+                    }); 
+                }
+            )
+    }
+
+    render() {
+        const { error, isLoaded, records } = this.state;
+        if (error) {
+            return <div>Error: {error.message}</div>
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            return (
+                <ul>
+                    {records.map(record => (
+                        <li key={record.id}>
+                            {record.title} {record.body}
+                        </li>
+                    ))}
+                </ul>
+            )
+        }
+    }
+}
+
+export default RecordsList;
